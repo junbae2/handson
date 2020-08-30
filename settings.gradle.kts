d598c4a26e45273e6c9346b6c1d4e0f0f50b4b6f
junbae.bae <bluesider@naver.com>
2020-08-31 02:58:54 +0900

    revise setting.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,9 +8,9 @@ fileTree(".") {
 }.map {
     relativePath(it.parent)
 }.forEach {
-    val project = it.replace(File.separator, ":")
+    val project = ":${it.substringAfterLast(File.separator)}"
     val projectDir = "$rootDir/$it"
     println("sub projects - $project, $projectDir")
     include(project)
-    project(":$project").projectDir = File(projectDir)
+    project("$project").projectDir = File(projectDir)
 }
--- spring/boot-jpa-kotlin/build.gradle.kts
+++ spring/boot-jpa-kotlin/build.gradle.kts
@@ -10,5 +10,18 @@ plugins {
     kotlin("plugin.spring") version Versions.kotlin
 
     id("org.springframework.boot") version Versions.springBoot
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
 }
+
+dependencies {
+//    "implementation"("org.springframework.boot:spring-boot-starter-data-jpa")
+//    "implementation"("org.springframework.boot:spring-boot-starter-mustache")
+//    "implementation"("org.springframework.boot:spring-boot-starter-web")
+//    "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin")
+//    "implementation"("org.jetbrains.kotlin:kotlin-reflect")
+//    "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+//    "runtimeOnly"("com.h2database:h2")
+//    "runtimeOnly"("org.springframework.boot:spring-boot-devtools")
+//    "testImplementation"("org.springframework.boot:spring-boot-starter-test")
+}
+
