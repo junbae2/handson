3f8c4bcb1f2ec489cad4caeaaf9f4a708443eeaf
junbae.bae <bluesider@naver.com>
2020-09-13 00:27:07 +0900

    revise Plugin dependencies in Dependency.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,23 @@
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.version
+import org.gradle.plugin.use.PluginDependenciesSpec
+
 object Versions {
     const val kotlin = "1.4.0"
     const val springBoot = "2.3.1.RELEASE"
+    const val springBootDependencyManager = "1.0.10.RELEASE"
+}
+
+fun PluginDependenciesSpec.kotlinSpringBootPlugin() {
+    kotlinPlugin()
+    id("org.springframework.boot") version Versions.springBoot
+    id("io.spring.dependency-management") version Versions.springBootDependencyManager
+}
+
+fun PluginDependenciesSpec.kotlinPlugin() {
+    kotlin("jvm")
+    kotlin("plugin.jpa") version Versions.kotlin
+    kotlin("plugin.spring") version Versions.kotlin
+    kotlin("kapt")
 }
 
--- spring/boot-jpa-kotlin/build.gradle.kts
+++ spring/boot-jpa-kotlin/build.gradle.kts
@@ -4,27 +4,20 @@ version = "1.0-SNAPSHOT"
 description = "boot-jpa-kotlin"
 
 plugins {
-
-    kotlin("jvm")
-    kotlin("plugin.jpa") version Versions.kotlin
-    kotlin("plugin.spring") version Versions.kotlin
-    kotlin("kapt")
-
-    id("org.springframework.boot") version Versions.springBoot
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    kotlinSpringBootPlugin()
 }
 
 sourceSets {
 }
 
 dependencies {
-//    "implementation"("org.springframework.boot:spring-boot-starter-data-jpa")
-//    "implementation"("org.springframework.boot:spring-boot-starter-mustache")
-//    "implementation"("org.springframework.boot:spring-boot-starter-web")
-//    "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin")
-//    "implementation"("org.jetbrains.kotlin:kotlin-reflect")
-//    "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-//    "runtimeOnly"("com.h2database:h2")
-//    "runtimeOnly"("org.springframework.boot:spring-boot-devtools")
-//    "testImplementation"("org.springframework.boot:spring-boot-starter-test")
+    "implementation"("org.springframework.boot:spring-boot-starter-data-jpa")
+    "implementation"("org.springframework.boot:spring-boot-starter-mustache")
+    "implementation"("org.springframework.boot:spring-boot-starter-web")
+    "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin")
+    "implementation"("org.jetbrains.kotlin:kotlin-reflect")
+    "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    "runtimeOnly"("com.h2database:h2")
+    "runtimeOnly"("org.springframework.boot:spring-boot-devtools")
+    "testImplementation"("org.springframework.boot:spring-boot-starter-test")
 }
