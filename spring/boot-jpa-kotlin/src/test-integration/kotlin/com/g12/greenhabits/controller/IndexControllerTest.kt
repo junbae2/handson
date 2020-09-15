2850ce5f27b3b8a2bcd296a4f8b037dcc0d43a95
junbae.bae <bluesider@naver.com>
2020-09-16 00:24:01 +0900

    add junit properties

--- spring/boot-jpa-kotlin/src/test-integration/kotlin/com/g12/greenhabits/controller/IndexControllerTest.kt
+++ spring/boot-jpa-kotlin/src/test-integration/kotlin/com/g12/greenhabits/controller/IndexControllerTest.kt
@@ -1,7 +1,9 @@
 package com.g12.greenhabits.controller
 
 import com.g12.greenhabits.GreenHabitsApplication
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,10 +20,21 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 )
 class IndexControllerTest(@Autowired val restTemplate: TestRestTemplate) {
 
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
     @Test
     fun `Assert index page title, content and status code` () {
         val entity = restTemplate.getForEntity<String>("/")
         assertTrue { entity.statusCode == HttpStatus.OK }
         assertTrue { entity.body?.contains("<h1>Green Habits</h1>") == true}
     }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
+    }
+
 }
