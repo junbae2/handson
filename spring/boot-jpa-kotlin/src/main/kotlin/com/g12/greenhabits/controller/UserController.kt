6912b1be70911a263bd6dd587a8c8ad3a80628d5
junbae bae <bluesider@gmail.com>
2020-09-27 01:47:50 +0900

    Implement User Rest Apis. 'findAll', 'findOne by email' (#6)
    Co-authored-by: junbae.bae <bluesider@naver.com>

--- spring/boot-jpa-kotlin/src/main/kotlin/com/g12/greenhabits/controller/UserController.kt
+++ spring/boot-jpa-kotlin/src/main/kotlin/com/g12/greenhabits/controller/UserController.kt
@@ -1,4 +1,21 @@
 package com.g12.greenhabits.controller
 
-class UserController {
+import com.g12.greenhabits.jpa.repository.UserRepository
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+
+@RestController
+@RequestMapping("/api/user")
+class UserController(val repository: UserRepository) {
+
+    @GetMapping
+    fun findAll() = repository.findAll()
+
+    @GetMapping("/{email}")
+    fun findOne(@PathVariable email: String) = repository.findByEmail(email)
+            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does net exist")
 }
---
+++ spring/boot-jpa-kotlin/src/test-integration/kotlin/com/g12/greenhabits/controller/UserControllerTest.kt
@@ -0,0 +1,35 @@
+package com.g12.greenhabits.controller
+
+import com.g12.greenhabits.jpa.entity.UserEntity
+import com.g12.greenhabits.jpa.repository.UserRepository
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import java.time.LocalDateTime
+
+@WebMvcTest
+class UserControllerTest(@Autowired val mockMvc: MockMvc) {
+
+    @MockkBean
+    private lateinit var userRepository: UserRepository
+
+    @Test
+    fun `List users`() {
+        val jbb = UserEntity("jbb@naver.com", "jbb", "jbb!", LocalDateTime.now())
+        val sjy = UserEntity("sjy@naver.com", "sjy", "sjy!", LocalDateTime.now())
+
+        every { userRepository.findAll() } returns listOf(jbb, sjy)
+
+        mockMvc.perform(get("/api/user/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("\$.[0].email").value(jbb.email))
+                .andExpect(jsonPath("\$.[1].email").value(sjy.email))
+    }
+}
