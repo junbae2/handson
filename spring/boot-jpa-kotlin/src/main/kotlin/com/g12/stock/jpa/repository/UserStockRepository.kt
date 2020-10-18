910af1311c1719c0307ea5993c66a3207ffd3e81
junbae.bae <bluesider@naver.com>
2020-10-19 00:05:57 +0900

    Add Unit Test of UserStockRepository

--- spring/boot-jpa-kotlin/src/main/kotlin/com/g12/stock/jpa/repository/UserStockRepository.kt
+++ spring/boot-jpa-kotlin/src/main/kotlin/com/g12/stock/jpa/repository/UserStockRepository.kt
@@ -4,5 +4,5 @@ import com.g12.stock.jpa.entity.UserStockEntity
 import org.springframework.data.repository.CrudRepository
 
 interface UserStockRepository : CrudRepository<UserStockEntity, Long> {
-    fun findByUserId(userId: Long): Iterable<UserStockEntity>
+    fun findByUserId(userId: Long): List<UserStockEntity>
 }
